85246ecf54814c661638ab9e958eda3d60542187
Yuyan <845058547@qq.com>
2019-09-30 23:06:56 +0800

    Fix a bug

--- src/main/java/com/github/hcsp/generics/OrderService.java
+++ src/main/java/com/github/hcsp/generics/OrderService.java
@@ -1,7 +1,8 @@
 package com.github.hcsp.generics;
 
-import java.util.List;
 import javax.inject.Inject;
+import java.util.LinkedList;
+import java.util.List;
 
 public class OrderService {
     private final OrderDao orderDao;
@@ -16,6 +17,9 @@ public class OrderService {
     // 请改正这里的编译错误
     public void processOrders() {
         List<Order> orders = orderDao.getAllOrders();
-        orderProcessService.sendOrders(orders);
+        //方案1-将orders设置为不可修改的.
+//        orderProcessService.sendOrders(Collections.unmodifiableList(orders));
+        //方案2-将orders转换为实现了List接口的类型例如ArrayList、LinkList
+        orderProcessService.sendOrders(new LinkedList<>(orders));
     }
 }
